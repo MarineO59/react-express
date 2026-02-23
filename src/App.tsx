93a0e902fe03cbbba8ecaca910f641bdb41d0e7d
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-02-23 14:56:26 +0100

    fix api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<any>(null)
 useEffect(() => {
   fetch("http://localhost:3310/api/employees")
     .then(response => response.json())
@@ -11,12 +11,16 @@ useEffect(() => {
 }, [])
 
 
-  return (
-    <>
-     
-      <p>{JSON.stringify(data)}</p>
-    </>
-  )
-}
+ return (
+  <>
+    {data && (
+      <div>
+        <p>{data.results[0].name.first} {data.results[0].name.last}</p>
+        <p>{data.results[0].email}</p>
+        <img src={data.results[0].picture.medium} />
+      </div>
+    )}
+  </>
+)
 
 export default App
